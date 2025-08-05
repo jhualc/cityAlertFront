19a3592b4becf1ffb278b9f580043b78de024951
HakiFC <36087368+HakiFC@users.noreply.github.com>
2025-08-05 17:39:33 -0500

    Update environment.prod.ts

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -2,7 +2,7 @@ export const environment = {
   production: true,
   HOST_BACKEND: '127.0.0.1',
   URL_SERVICIOS: "/api/api",
-  URL_BACKEND: "https://cityalertapi.azurewebsites.net",
+  URL_BACKEND: "https://cityalertapi-dev.azurewebsites.net",
   URL_FRONTEND: "/",
   URL_FILESERVER: "/api/public",
   SSL_WSS: false,
